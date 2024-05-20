24b57a0f253357e2ca4f13a2fabb47c527ccf61a
Merahimi <mohammaderahimi80@gmail.com>
2024-05-20 11:33:05 +0330

    chnaging homepage name

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import CarouselSlider from "../component/CarouselSlider";
 import Slider01 from "../../public/image/Slider/Slider01.jpg";
 import Slider02 from "../../public/image/Slider/Slider02.jpg";
 import Slider03 from "../../public/image/Slider/Slider03.jpg";
-const Homepage = () => {
+const HomePage = () => {
   return (
     <Fragment>
       <CarouselSlider Count={3} Slider={[Slider01, Slider02, Slider03]} />
@@ -11,4 +11,4 @@ const Homepage = () => {
   );
 };
 
-export default Homepage;
+export default HomePage;
--- src/route/rouates.tsx
+++ src/route/rouates.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layout";
-import Homepage from "../pages/Homepage";
+import HomePage from "../pages/HomePage";
 import AboutUs from "../pages/AboutUs";
 import ContactUs from "../pages/ContactUs";
 import ErrorPage from "./ErrorPage";
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/Digi-Shop", element: <Homepage /> },
+      { path: "/Digi-Shop", element: <HomePage /> },
       { path: "/Digi-Shop/about", element: <AboutUs /> },
       { path: "/Digi-Shop/contact", element: <ContactUs /> },
       { path: "/Digi-Shop/product", element: <ProductPage/> },
