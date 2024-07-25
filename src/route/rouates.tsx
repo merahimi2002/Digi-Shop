ce86eff89ca5d6bfa2232d3cde605ab7454b376a
MErahimi <mohammaderahimi80@gmail.com>
2024-07-25 13:10:45 +0330

    testing

--- src/route/rouates.tsx
+++ src/route/rouates.tsx
@@ -3,16 +3,16 @@ import Layout from "./layout";
 import Home from "../pages/Home";
 import AboutUs from "../pages/AboutUs";
 import ContactUs from "../pages/ContactUs";
-// import ErrorPage from "./ErrorPage";
+import ErrorPage from "./ErrorPage";
 import ProductPage from "../pages/ProductPage";
 import Product from "../component/Product/Product";
 import CategoriyItem from "../component/Category/CategoryItem";
 
 const router = createBrowserRouter([
   {
-    path: "/Digi-Shop/",
+    path: "/",
     element: <Layout />,
-    // errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/Digi-Shop/", element: <Home /> },
       { path: "/Digi-Shop/about", element: <AboutUs /> },
